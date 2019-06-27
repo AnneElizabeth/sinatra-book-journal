efe1e853e5b7d4b8e4bca449b34f1c2119f65ede
github username <github email address>
2019-06-26 22:27:43 -0600

    reorder book_title and content columns

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@
 ActiveRecord::Schema.define(version: 20190623063234) do
 
   create_table "journal_entries", force: :cascade do |t|
-    t.string   "content"
     t.string   "book_title"
+    t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
