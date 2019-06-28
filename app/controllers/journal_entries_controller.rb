076d05b65e47e6bc1ae5176cfc3e98f2498a784b
github username <github email address>
2019-06-27 22:29:18 -0600

    create working edit feature and add feature preventing users from editing/adding/deleting the entries of other users.

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -22,30 +22,43 @@ class JournalEntriesController < ApplicationController
 
     #dynamic show route for journal entries
     get '/journal_entries/:id' do
-        find_journal_entry
+        find_entry
         erb :'/journal_entries/show'
     end
 
     #edit routes
     get '/journal_entries/:id/edit' do
-        find_journal_entry
-        erb :'/journal_entries/edit'
+        find_entry
+        if logged_in?
+            if @journal_entry.user == current_user
+                erb :'/journal_entries/edit'
+            else
+                redirect "users/#{current_user.id}"
+            end
+        else
+            redirect "/"
+        end
     end
 
     patch '/journal_entries/:id' do
-        #find journal entry
-        find_journal_entry
-        #modify/update journal entry
-        @journal_entry.update(book_title: params[:book_title], content: params[:content]) #ActiveRecord method
-        #redirect to show page
-        redirect "/journal_entries/#{@journal_entry.id}"
+        find_entry
+        if logged_in?
+            if @journal_entry.user == current_user
+                @journal_entry.update(book_title: params[:book_title], content: params[:content]) 
+                redirect "/journal_entries/#{@journal_entry.id}"
+            else
+                redirect "users/#{current_user.id}"
+            end
+        else
+            redirect "/"
+        end
     end
 
     #index route for all journal entries
 
     #helper method to prevent code duplication
     private #this method will only be used here
-    def find_journal_entry
+    def find_entry
         @journal_entry = JournalEntry.find(params[:id])
     end
 
